bb7c336d6516ca91e3712549594fa009a87b8c6c
Jose Alberto Sanchez Pacheco <jasp@ibm.com>
2022-05-02 19:05:10 -0500

    schema fixed?

---
+++ target/classes/database/postgresql/schema-postgresql.sql
@@ -0,0 +1,18 @@
+
+/*create database tarjetasapi; */
+
+CREATE SCHEMA tarjetas_schema;
+create table tarjetas_schema.tarjetas
+(
+    id_tarjeta     bigint           not null
+        primary key,
+    nombre_tarjeta varchar(255)     not null,
+    preferencia    varchar(255)     not null,
+    edad_min       integer          not null,
+    edad_max       integer          not null,
+    salario_min    double precision not null,
+    salario_max    double precision
+);
+
+alter table tarjetas_schema.tarjetas
+    owner to postgres;
